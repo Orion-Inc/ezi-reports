5799e96990be2676cc3ebcc5bbafb8b63626e7f7
teejay13 <adetunjisalako@gmail.com>
2017-11-01 17:57:23 +0000

    backend add student

--- app/views/app-student.php
+++ app/views/app-student.php
@@ -9,5 +9,41 @@
     </div>
 </div>
 <div class="page-content container-fluid animated fadeIn">
-    
+    <form>
+    	<h1>students</h1>
+    </form>
 </div>
+
+
+<script type="text/javascript">
+    $(".app-form").unbind('submit').bind('submit', function(){
+        var form = $(this);
+        var data = form.serialize();
+        var url = form.attr('action');
+
+        $.ajax({
+            url:url,
+            dataType:'json',
+            type:'POST',
+            data:data,
+            beforeSend : function(){
+                alert();
+            },
+            success:function(data){
+                if (data.error != 'false') {
+                    toastr.error(data.message, 'Error!');
+                    $('#page-content').load();
+                }else{
+                    toastr.success(data.message, 'Success!');
+                    $('#page-content').load();
+                }
+            }
+        });
+
+        return false;
+    });
+
+    //
+</script>
+
+
