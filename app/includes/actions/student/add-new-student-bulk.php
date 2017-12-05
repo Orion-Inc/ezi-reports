5efe5d077b3c882608a14d26af50ae414074b513
Farid Adam <faridibin@gmail.com>
2017-12-05 18:41:38 +0000

    Bulk Insertion

--- app/includes/actions/student/add-new-student-bulk.php
+++ app/includes/actions/student/add-new-student-bulk.php
@@ -4,7 +4,20 @@
 
 	$errors = array();
 
-	if (!empty($_FILES['bulk_student_file'])) {
+	if (!empty($_FILES['bulk_student_file']) && isset($_FILES['bulk_student_file']['name'])) {
+
+		$file = fopen($_FILES['bulk_student_file']['tmp_name'],"r");
+
+		while (!feof($file)) {
+			$data = fgetcsv($file);
+			print_r($data);
+		}
+
+
+        
+
+
+        $response = '';
 
 	}else{
 		$response = array('error' => 'true', 'url' => 'student', 'message' => "An Error Occurred! Please <a href=\"javascript:page('student')\" data-dismiss=\"modal\">Try again.</a>");
