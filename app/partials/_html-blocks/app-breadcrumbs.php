c805c22fe14e16290a22b1eb1bcaf141c3de575e
Farid Adam <faridibin@gmail.com>
2017-12-10 10:22:37 +0000

    Admin View Templates Updates

--- app/partials/_html-blocks/app-breadcrumbs.php
+++ app/partials/_html-blocks/app-breadcrumbs.php
@@ -1,11 +1,11 @@
-<?php if (isset($_GET['dashboard'])): ?>
-	<h4 class="mt-0 mb-5"><i class="ion-speedometer"></i> Dashboard</h4>
+<?php if (isset($_GET['dashboard']) || isset($_GET['admin-dashboard'])): ?>
+	<h4 class="mt-0 mb-5"><i class="ti-dashboard"></i> Dashboard</h4>
 	<ol class="breadcrumb mb-0">
 	    <li class="active">{Description}</li>
 	</ol>
 <?php endif ?>
 
-<?php if (isset($_GET['school'])): ?>
+<?php if (isset($_GET['school']) || isset($_GET['admin-school'])): ?>
 	<h4 class="mt-0 mb-5"><i class="ti-home"></i> School</h4>
 	<ol class="breadcrumb mb-0">
 	    <li class="active">{Description}</li>
@@ -33,14 +33,21 @@
 	</ol>
 <?php endif ?>
 
-<?php if (isset($_GET['reports'])): ?>
+<?php if (isset($_GET['admin-subjects'])): ?>
+	<h4 class="mt-0 mb-5"><i class="ti-book"></i> Subjects</h4>
+	<ol class="breadcrumb mb-0">
+	    <li class="active">{Description}</li>
+	</ol>
+<?php endif ?>
+
+<?php if (isset($_GET['reports']) || isset($_GET['admin-reports'])): ?>
 	<h4 class="mt-0 mb-5"><i class="ti-files"></i> Reports</h4>
 	<ol class="breadcrumb mb-0">
 	    <li class="active">{Description}</li>
 	</ol>
 <?php endif ?>
 
-<?php if (isset($_GET['search'])): ?>
+<?php if (isset($_GET['search']) || isset($_GET['admin-search'])): ?>
 	<h4 class="mt-0 mb-5"><i class="ti-search"></i> Search</h4>
 	<ol class="breadcrumb mb-0">
 	    <li class="active">{Description}</li>
