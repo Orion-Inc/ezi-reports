af2ced7ad91862d8bcbd7f79df5796090e0c9e35
Farid Adam <faridibin@gmail.com>
2018-04-13 20:27:14 +0000

    Update

--- app/includes/auth/reset-password.php
+++ app/includes/auth/reset-password.php
@@ -1,6 +1,23 @@
 <?php
-	require_once 'Autoloader.php'; 
-	$generate = new App(); 
+	require_once 'Autoloader.php';
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require '../PHPMailer/src/Exception.php';
+	require '../PHPMailer/src/PHPMailer.php';
+	require '../PHPMailer/src/SMTP.php';
+
+	$generate = new App();
+	$mail = new PHPMailer(true);
+
+	//Server settings                               
+	$mail->isSMTP();                                      // Set mailer to use SMTP
+	$mail->Host = 'smtp.ipage.com';                       // Specify main and backup SMTP servers
+	$mail->SMTPAuth = true;                               // Enable SMTP authentication
+	$mail->Username = 'sms@orionic.tech';                 // SMTP username
+	$mail->Password = 'sms@Oriondope$1000';               // SMTP password
+	// $mail->SMTPSecure = 'tls';                         // Enable TLS encryption, `ssl` also accepted
+	$mail->Port = 587;                                    // TCP port to connect to
 
 	$errors = array();
 
@@ -29,20 +46,38 @@
 				$mailTemplate = file_get_contents('../../templates/emails/password-reset.html');
 
 				$params = array(
-					'{$user}' => $school['school_name'],
-					'{$verification_code}' => $verificationCode
+					'{user}' => $school['school_name'],
+					'{verification_code}' => $verificationCode
 				);
 				$email = strtr($mailTemplate, $params);
+				echo $email;
+
+				try {
+				//Recipients
+					$mail->setFrom('sms@orionic.tech', 'Orion Ltd.');
+					$mail->addAddress($email, $fullname);
+					$mail->addReplyTo('sms@orionic.tech', 'Orion Ltd.');
+				
+				//Content
+					$mail->isHTML(true);                                  // Set email format to HTML
+					$mail->Subject = 'Recover Password';
+					$mail->Body = $body;
+					$mail->AltBody = 'To recover your password, please click on this link: ' . $recover_link;
+
+					$mail->send();
+				} catch (Exception $e) {
+					$errors[] = array('message' => 'Message could not be sent.');
+				}
 
 				if (1==1){
 					
 
 
-					header("Location:../../../app/auth/{$prefix}{$token}&type=email");
+					//header("Location:../../../app/auth/{$prefix}{$token}&type=email");
 				}else{
 					$errors[0] = array('auth_error' => 'true', 'message' => "We encountered a problem while trying to send you a verification code.\nPlease try again or Contact Us.");
 					$_SESSION['ERRORS'] = $errors[0];
-					header("Location:../../../app/auth/?auth=forgot-password");
+					//header("Location:../../../app/auth/?auth=forgot-password");
 				}
 			}
 		}else if(preg_match("/[S][0-99]*/",$loginPrefix)===1){
@@ -51,14 +86,14 @@
 			if (empty($isStudentValid)) {
 				$errors[0] = array('auth_error' => 'true', 'message' => "The User Code you entered is invalid!");
 				$_SESSION['ERRORS'] = $errors[0];
-				header("Location:../../../app/auth/?auth=forgot-password");
+				//header("Location:../../../app/auth/?auth=forgot-password");
 			} else {
 				$isTelValid = Database::query("SELECT `guardian_telephone` FROM `ezi_student_guardian` WHERE `student_code` ='{$code_email}'")[0];
 
 				if (empty($isTelValid)) {
 					$errors[0] = array('auth_error' => 'true', 'message' => "We encountered a problem while trying to send you a verification code.\nPlease try again or Contact Us.");
 					$_SESSION['ERRORS'] = $errors[0];
-					header("Location:../../../app/auth/?auth=forgot-password");
+					//header("Location:../../../app/auth/?auth=forgot-password");
 				} else {
 					$student = $isStudentValid;
 					$token = $generate->token_generator();
@@ -71,23 +106,23 @@
 						'{$verification_code}' => $verificationCode
 					);
 					$sms = strtr($smsTemplate, $params);
-
+					echo $sms;
 					if (1==1){
 						
 
 
-						header("Location:../../../app/auth/{$prefix}{$token}&type=sms");
+						//header("Location:../../../app/auth/{$prefix}{$token}&type=sms");
 					}else{
 						$errors[0] = array('auth_error' => 'true', 'message' => "We encountered a problem while trying to send you a verification code.\nPlease try again or Contact Us.");
 						$_SESSION['ERRORS'] = $errors[0];
-						header("Location:../../../app/auth/?auth=forgot-password");
+						//header("Location:../../../app/auth/?auth=forgot-password");
 					}
 				}
 			}			
 		}else{
 			$errors[0] = array('auth_error' => 'true', 'message' => "The Email or User Code you entered is invalid!");
 			$_SESSION['ERRORS'] = $errors[0];
-			header("Location:../../../app/auth/?auth=forgot-password");
+			//header("Location:../../../app/auth/?auth=forgot-password");
 		}
 	}
 ?>
