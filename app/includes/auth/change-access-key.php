1fc3c1dc77b5cb974a4659569b64317e375972ab
Farid Adam <faridibin@gmail.com>
2018-04-14 10:56:11 +0000

    Update

--- app/includes/auth/change-access-key.php
+++ app/includes/auth/change-access-key.php
@@ -3,68 +3,74 @@
     $transact = Database::connect();
 
 	$errors = array();
+    $data = $_POST;
+
     
-    switch ($_SESSION['SESS_USER_TYP']) {
-        case 'school':
-            $url = "settings";
-            break;
-        case 'student':
-            $url = "student-settings";
-            break;
-        case 'eziAdmin':
-            $url = "admin-settings";
-            break;
+    if(isset($data['user']['code']) && isset($data['user']['token'])){
+
+    }else{
+        switch ($_SESSION['SESS_USER_TYP']) {
+            case 'school':
+                $url = "settings";
+                break;
+            case 'student':
+                $url = "student-settings";
+                break;
+            case 'eziAdmin':
+                $url = "admin-settings";
+                break;
+        }
+        
+        $authParams = array( 
+            'user_code' => stripslashes($data['user_code']),
+            'oldAccessKey' => sha1($data['oldAccessKey']),
+            'newAccessKey' => sha1($data['newAccessKey'])
+        );
+
+        switch ($_SESSION['SESS_USER_TYP']) {
+            case 'school':
+            case 'student':
+                $transact->beginTransaction();
+                try {
+                    $updateAccessKey = Database::query("UPDATE `ezi_users` SET 
+                        `access_key`= :newAccessKey
+                        WHERE 
+                        `user_code`= :user_code
+                        AND 
+                        `access_key`= :oldAccessKey", 
+                        $authParams
+                    );
+
+                    $transact->commit();
+                    $response = array('error' => 'false', 'url' => $url, 'message' => "Access Key Changed Successfully!");
+                } catch (PDOException $e) {
+                    $transact->rollBack();
+                    $errors[] = $e->getMessage();
+                    $response = array('error' => 'true', 'error_msg' => $errors[0], 'url' => $url, 'message' => "An Error Occurred While Trying To Change Access Key");
+                }
+                break;
+            case 'eziAdmin':
+                $transact->beginTransaction();
+                try {
+                    $updateAccessKey = Database::query("UPDATE `ezi_admin` SET 
+                        `password`= :newAccessKey
+                        WHERE 
+                        `id`= :user_code
+                        AND 
+                        `password`= :oldAccessKey", 
+                        $authParams
+                    );
+
+                    $transact->commit();
+                    $response = array('error' => 'false', 'url' => $url, 'message' => "Password Changed Successfully!");
+                } catch (PDOException $e) {
+                    $transact->rollBack();
+                    $errors[] = $e->getMessage();
+                    $response = array('error' => 'true', 'error_msg' => $errors[0], 'url' => $url, 'message' => "An Error Occurred While Trying To Change Password");
+                }
+                break;
+        }
+        echo json_encode($response);
     }
-    
-	$authParams = array( 
-		'user_code' => stripslashes($_POST['user_code']),
-        'oldAccessKey' => sha1($_POST['oldAccessKey']),
-        'newAccessKey' => sha1($_POST['newAccessKey'])
-    );
-
-    switch ($_SESSION['SESS_USER_TYP']) {
-        case 'school':
-        case 'student':
-            $transact->beginTransaction();
-            try {
-                $updateAccessKey = Database::query("UPDATE `ezi_users` SET 
-                    `access_key`= :newAccessKey
-                    WHERE 
-                    `user_code`= :user_code
-                    AND 
-                    `access_key`= :oldAccessKey", 
-                    $authParams
-                );
-
-                $transact->commit();
-                $response = array('error' => 'false', 'url' => $url, 'message' => "Access Key Changed Successfully!");
-            } catch (PDOException $e) {
-                $transact->rollBack();
-                $errors[] = $e->getMessage();
-                $response = array('error' => 'true', 'error_msg' => $errors[0], 'url' => $url, 'message' => "An Error Occurred While Trying To Change Access Key");
-            }
-            break;
-        case 'eziAdmin':
-            $transact->beginTransaction();
-            try {
-                $updateAccessKey = Database::query("UPDATE `ezi_admin` SET 
-                    `password`= :newAccessKey
-                    WHERE 
-                    `id`= :user_code
-                    AND 
-                    `password`= :oldAccessKey", 
-                    $authParams
-                );
-
-                $transact->commit();
-                $response = array('error' => 'false', 'url' => $url, 'message' => "Password Changed Successfully!");
-            } catch (PDOException $e) {
-                $transact->rollBack();
-                $errors[] = $e->getMessage();
-                $response = array('error' => 'true', 'error_msg' => $errors[0], 'url' => $url, 'message' => "An Error Occurred While Trying To Change Password");
-            }
-            break;
-    }
-	echo json_encode($response);
 ?>
 
--- app/includes/auth/verify-code.php
+++ app/includes/auth/verify-code.php
@@ -6,7 +6,6 @@
 
     $data = $_POST;
 
-    $dir = '/ezi-reports/app';
 
     if (empty($data['user']) || empty($data['verify_code'])) {
         $errors[0] = array('auth_error' => 'true', 'message' => "An error occured. Please contact Us.");
@@ -16,9 +15,41 @@
         $verify_code = $data['verify_code'];
 
         $isUserValid = Database::query("SELECT `user_code`,`token` FROM `ezi_user_password_resets` WHERE `verification_code` ='{$verify_code}'")[0];
-
         
-        print_r($isUserValid);
+        if($data['user']['token'] != $isUserValid['token']){
+            $errors[0] = array('auth_error' => 'true', 'message' => "Make sure you entered the correct Verification Code.");
+            $_SESSION['ERRORS'] = $errors[0];
+            header("Location: {$_SERVER['HTTP_REFERER']}");
+        }else {
+            $user_code = $isUserValid['user_code'];
+            $token = $isUserValid['token'];
+
+            $transact->beginTransaction();
+            try {
+                $verifyParams = array(
+                    'token' => $token,
+                    'verification_code' => $verify_code
+                );
+
+                $verifyQuery = Database::query(
+                "DELETE FROM `ezi_user_password_resets` 
+                        WHERE 
+                    `token` = :token
+                        AND 
+                    `verification_code` = :verification_code",
+                    $verifyParams
+                );
+
+                $transact->commit();
+                header("Location:../../../app/auth/?auth=access-code&x={$user_code}&y={$token}");
+            } catch (PDOException $e) {
+                $transact->rollBack();
+                $errors[0] = array('auth_error' => 'true', 'error_msg' => $e->getMessage(), 'message' => "We encountered a problem while trying to verify your code.\nPlease try again or Contact Us.");
+                $_SESSION['ERRORS'] = $errors[0];
+                header("Location:../../../app/auth/?auth=forgot-password");
+            }
+        }
+        
     }
 
 ?>
